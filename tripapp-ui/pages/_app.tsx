3333faec89ba3a8a0d8d96f7860493f0e81e98b2
NOC3 <alenoce1999@gmail.com>
2022-12-27 16:59:48 +0100

    creating branch

--- tripapp-ui/pages/_app.tsx
+++ tripapp-ui/pages/_app.tsx
@@ -4,6 +4,10 @@ import {SessionProvider} from 'next-auth/react';
 import PageWrapper from '../components/pageWrapper';
 
 export default function App({Component, pageProps}: AppProps) {
+
+
+
+
     return (
         <SessionProvider session={pageProps.session}>
             <PageWrapper>
