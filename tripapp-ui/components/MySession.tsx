80ea105a796199399195e28343a21ffb21ba1ae2
NOC3 <alenoce1999@gmail.com>
2022-12-28 20:42:58 +0100

    feature context

---
+++ tripapp-ui/components/MySession.tsx
@@ -0,0 +1,33 @@
+import React, {createContext, useContext, useState} from "react";
+import {signIn, useSession} from "next-auth/react";
+import SingInMain from "./SingIn";
+
+const UserContext:any = createContext<any>();
+
+export default function MySession({children}:{children: React.ReactNode}){
+
+    //check sessione, se si child, se no sigin
+    const {data : session} = useSession();
+
+    //variabilicontext
+    const [actTravel, setActTravel] = useState<string>("culo");
+
+    //actual context
+    let state = {
+        actTravel: actTravel,
+        setActTravel:setActTravel
+    }
+
+
+
+    return session? <>
+        <UserContext.Provider value={state}>
+            {children}
+        </UserContext.Provider>
+        </> :
+        <SingInMain/>
+}
+
+export function useAppContext() {
+    return useContext(UserContext);
+}
--- tripapp-ui/pages/_app.tsx
+++ tripapp-ui/pages/_app.tsx
@@ -2,13 +2,33 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SessionProvider} from 'next-auth/react';
 import PageWrapper from '../components/pageWrapper';
+import Head from "next/head";
+import React, {createContext, FC, useContext, useState} from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
+import {NextPageContext} from "next";
+import MySession from "../components/MySession";
+
+
 
 export default function App({Component, pageProps}: AppProps) {
+
     return (
-        <SessionProvider session={pageProps.session}>
-            <PageWrapper>
-                <Component {...pageProps} />
-            </PageWrapper>
-        </SessionProvider>
+        <>
+            <Head>
+                <title>TripApp</title>
+                <meta name="description" content="TripApp - Take a trip!"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <SessionProvider session={pageProps.session}>
+                <MySession>
+                    <Component {...pageProps} culo={"culo"}/>
+                </MySession>
+            </SessionProvider>
+        </>
     );
 }
+
+
+
+
+
