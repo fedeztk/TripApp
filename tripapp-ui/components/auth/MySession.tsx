8238b546831a0148a45daac0a57df044c2848426
NOC3 <alenoce1999@gmail.com>
2022-12-30 18:27:56 +0100

    test rpi npm run dev

---
+++ tripapp-ui/components/auth/MySession.tsx
@@ -0,0 +1,32 @@
+import React, {createContext, useContext, useState} from "react";
+import {signIn, useSession} from "next-auth/react";
+import SingInMain from "./SingIn";
+
+const UserContext:any = createContext<any>(undefined);
+
+export default function MySession({children}:{children: React.ReactNode}){
+
+    const {data : session} = useSession();
+
+    const [data, setData] = useState<any>();
+    let state = {
+        data: data,
+        setData:setData
+    }
+
+
+
+    if(!session){
+        return <SingInMain/>
+    }else{
+        return<>
+            <UserContext.Provider value={state}>
+                {children}
+            </UserContext.Provider>
+        </>
+    }
+}
+
+export function useAppContext() {
+    return useContext(UserContext);
+}
--- tripapp-ui/pages/_app.tsx
+++ tripapp-ui/pages/_app.tsx
@@ -2,16 +2,38 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SessionProvider} from 'next-auth/react';
 import PageWrapper from '../components/pageWrapper';
+<<<<<<< HEAD
 import {montserrat} from '../lib/theme';
+=======
+import Head from "next/head";
+import React, {createContext, FC, useContext, useState} from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
+import {NextPageContext} from "next";
+import MySession from "../components/auth/MySession";
+
+
 
 export default function App({Component, pageProps}: AppProps) {
-    return (
-        <SessionProvider session={pageProps.session}>
-            <PageWrapper>
-                <main className={montserrat.className}>
-                    <Component {...pageProps} />
-                </main>
-            </PageWrapper>
-        </SessionProvider>
-    );
+
+
+        return <>
+            <Head>
+                <title>TripApp</title>
+                <meta name="description" content="TripApp - Take a trip!"/>
+                <link rel="icon" href="/palm.ico"/>
+                <link rel="manifest" href="/manifest.json" />
+            </Head>
+            <SessionProvider session={pageProps.session}>
+                <MySession>
+                    <Component {...pageProps} culo={"culo"}/>
+                </MySession>
+            </SessionProvider>
+        </>
+
 }
+
+
+
+
+
+
