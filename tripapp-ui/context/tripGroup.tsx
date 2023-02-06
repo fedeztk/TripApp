08a4ae52c18ebc4420ddb91278723e67002b49cc
fedeztk <federicoserranexus@gmail.com>
2023-02-06 14:51:39 +0100

    fix(ui): persisted trip group context to local storage (surivives page refreshes)

--- tripapp-ui/context/tripGroup.tsx
+++ tripapp-ui/context/tripGroup.tsx
@@ -1,10 +1,26 @@
-import {createContext, useContext, useState} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
 import TripGroup from "../types/tripGroup";
 
 const Context = createContext<[TripGroup | null, (tripGroup: TripGroup | null) => void]>([null, () => {}]);
 
 export function TripGroupProvider({children}: { children: React.ReactNode }) {
     const [tripGroup, setTripGroup] = useState<TripGroup | null>(null);
+
+    useEffect(() => {
+        const group = localStorage.getItem('tripGroup');
+        if (group) {
+            setTripGroup(JSON.parse(group));
+        }
+    }, []);
+
+    useEffect(() => {
+        if (tripGroup) {
+            localStorage.setItem('tripGroup', JSON.stringify(tripGroup));
+        } else {
+            localStorage.removeItem('tripGroup');
+        }
+    }, [tripGroup]);
+
     return <Context.Provider value={[tripGroup, setTripGroup]}> {children} </Context.Provider>;
 }
 
