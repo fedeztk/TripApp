6ac6ffbf9f1cbb164ffb832ce0070e5fcaa9c2c4
fedeztk <58485208+fedeztk@users.noreply.github.com>
2023-02-17 12:25:38 +0100

    Feat(UI)/info (#35)
    * feat(ui):new info page
    
    * fix(ui): graphical enhancement
    
    * fix(ui): re-added the iso for the country
    
    * feat(ui): added new table of content + small fixes
    
    * fix(ui): small graphical adjustment
    
    * fix(ui): json now works with currency + graphical fixes
    
    * fix(ui):support for medium sized devices
    
    ---------
    
    Co-authored-by: Oselium <alex.andreose@gmail.com>

--- tripapp-ui/pages/[groupID]/info.tsx
+++ tripapp-ui/pages/[groupID]/info.tsx
@@ -1,12 +1,188 @@
+import {useTripGroupContext} from "../../context/tripGroup";
+import {Grid, useTheme} from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {useSession} from "next-auth/react";
+import LoadingPage from "../../components/loadingPage";
+import Stack from "@mui/material/Stack";
+import * as React from "react";
+import useSWR from "swr";
+import Member from "../../types/member";
+import {Informations} from "../../types/informations";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
+import FireTruckIcon from '@mui/icons-material/FireTruck';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import {useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
+import MapIcon from '@mui/icons-material/Map';
 
 export default function Info() {
-    // display the groupID in the URL
-    // const router = useRouter();
-    // const {groupID} = router.query;
-    return (
-        <Typography variant="h4">
-            Info
-        </Typography>
-    );
+    const [tripGroup] = useTripGroupContext()
+    const theme = useTheme()
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+    const {data: session} = useSession()
+    const groupId: string|undefined = tripGroup?.id.toString()
+    const userIdList : (string[] | undefined) = tripGroup?.members.map((m:Member)=> m.userId).filter((m:string)=> m !== session?.user?.id)
+
+    const path = "/v1/infos/"
+    const req = path + tripGroup?.iso
+    const {data, error, isLoading} = useSWR([req, "GET", session])
+    const [informations, setInformations] = useState<Informations|undefined>()
+
+    function currencyLoader() {
+        const currencies = informations?.info.currencies;
+        if (!currencies) {
+            return "-";
+        }
+        const currencyKeys = Object.keys(currencies) as Array<
+            keyof typeof currencies
+        >;
+
+        const firstCurrencyKey = currencyKeys[0];
+        const currencyInfo = currencies[firstCurrencyKey];
+        return currencyInfo.name.concat(" ("+currencyInfo.symbol+")");
+    }
+
+    function dataAnalizer() {
+        if (data !== undefined) {
+            setInformations(data)
+        }
+    }
+
+    const divStyle = {
+        width: '400px',
+        height: '100px',
+        padding: '10px'
+    };
+
+    useEffect(dataAnalizer, [data])
+    return showInfoCountry()
+
+    function showInfoCountry(){
+        return isLoading ? <LoadingPage/>
+        : (
+            <>
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="stretch"
+                spacing={1}>
+
+                <Typography variant="h4">
+                    <div style={divStyle}>
+                        <img src={informations?.info.flags.svg} style={{width: "20%", height: "50%", float: "left"}} />{informations?.info.names.common.toString().toUpperCase()}
+                    </div>
+                </Typography>
+
+                <Grid container spacing={1}>
+                    <Grid item xs={12} md={5}>
+                        <List>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <MedicalInformationIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Ambulance
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;{informations?.numbers.datas.ambulance.all?.toString() === "" ? "-" : informations?.numbers.datas.ambulance.all?.toString()}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <LocalPoliceIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Police
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;{informations?.numbers.datas.police.all?.toString() === "" ? "-" : informations?.numbers.datas.police.all?.toString()}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <FireTruckIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Fire Dept
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;{informations?.numbers.datas.fire.all?.toString() === "" ? "-" : informations?.numbers.datas.fire.all?.toString()}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                        </List>
+                    </Grid>
+                    {/*----------------------------------------------------------------*/}
+                    <Grid item xs={12} md={7}>
+                        <List>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <ApartmentIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Capital
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;{informations?.info.capital}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <MapIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Maps
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;<a href={informations?.info.maps.googleMaps} rel="noreferrer" target="_blank">OPEN</a>
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ width: 55, height: 55}}>
+                                        <MonetizationOnIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText>
+                                    <Typography variant="button" sx={{fontSize: '1.25em'}} gutterBottom>
+                                        &nbsp;&nbsp;Currency
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        &nbsp;&nbsp;&nbsp;{currencyLoader()}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+            </Stack>
+            </>
+        )
+    }
 }
